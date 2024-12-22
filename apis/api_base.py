6f5507591c2d0d46f3408b0a82790d2b72c5b7d4
jay4246 <jay.seo@101.inc>
2024-12-22 20:53:53 +0900

    Update api_base.py

--- apis/api_base.py
+++ apis/api_base.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from apis.version1 import comm_router
-from apis.version1 import survay_router
+from apis.version1 import survey_router
 
 api_router = APIRouter()
 
@@ -13,7 +13,7 @@ api_router.include_router(
 
 # comm_router의 API를 "/comm" 경로에 포함
 api_router.include_router(
-    survay_router.router,  # comm_router의 router 객체
+    survey_router.router,  # comm_router의 router 객체
     prefix="/survay",  # 모든 경로에 /comm 추가
     tags=["survay"]  # OpenAPI 문서에서 comm 관련 API로 태그
 )
